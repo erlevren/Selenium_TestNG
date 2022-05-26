816236d316a8b4c2da4b5a364e3ba893fb5c808c
Erol Evren <erlevren@gmail.com>
2022-05-27 00:07:50 +0300

    27/05

---
+++ src/test/java/day16Notations/C04DependsOnMethods.java
@@ -0,0 +1,49 @@
+package day16Notations;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C04DependsOnMethods  {
+    WebDriver driver;
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+        //amazona gidelim
+        driver.get("https://www.amazon.com");
+    }
+    @Test (dependsOnMethods = "test01")
+    public void test02() {
+        //nutella aratalim
+        WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+    }
+    @Test (dependsOnMethods = "test02")
+    public void test03() {
+        //Sonuc yazisinin nutella icerdigini test edelim
+        WebElement sonucYazisi = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        Assert.assertTrue(sonucYazisi.getText().contains("Nutella"));
+    }
+}
---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public abstract class TestBase {
+    // abstract yapmamizin sebebi, bu class'dan obje uretilmesinin onune gecmektir. Sart degildir
+    protected static WebDriver driver;
+    protected static String tarih;
+
+    @BeforeMethod
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("YYMMddHHmmss");
+        tarih = date.format(formatter);
+    }
+    @AfterMethod
+    public static void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+}
